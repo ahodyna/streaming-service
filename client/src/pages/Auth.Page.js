e52114a57527ee7deb659d0f8f1f4031760c5f73
alina <hodyna.alina@gmail.com>
2021-09-23 13:17:26 +0300

    implemented register features

--- client/src/pages/Auth.Page.js
+++ client/src/pages/Auth.Page.js
@@ -1,37 +1,48 @@
 import React, { useState } from 'react';
+import { useHttp } from '../hooks/http.hook';
 
 export const AuthPage = () => {
-    
+    const { loading, error, request } = useHttp()
+
     const [form, setForm] = useState({
         email: '', password: ''
-    })
+    });
+
+    const registerHandler = async () => {
+        try {
+            const data = await request('/api/auth/register', 'POST', {...form})
+            console.log('data', data)
+        } catch (e) {
+
+        }
+    }
 
     const changeHandler = event => {
-        setForm({...form, [event.target.name]: event.target.value})
+        setForm({ ...form, [event.target.name]: event.target.value })
     }
 
     return (
         <div>
             <h2>Authorization</h2>
-            <input 
-            placeholder='Enter email'
-            id='email'
-            type='text'
-            name='email'
-            onChange = {changeHandler}
+            <input
+                placeholder='Enter email'
+                id='email'
+                type='text'
+                name='email'
+                onChange={changeHandler}
             />
-             <input 
-            placeholder='Enter password'
-            id='password'
-            type='password'
-            name='password'       
-            onChange = {changeHandler}
+            <input
+                placeholder='Enter password'
+                id='password'
+                type='password'
+                name='password'
+                onChange={changeHandler}
             />
             <div>
-                <button>Login</button>
-                <button>Register</button>
+                <button disabled={loading}>Login</button>
+                <button onClick={registerHandler} disabled={loading}>Register</button>
             </div>
-            
+
         </div>
     )
 };
