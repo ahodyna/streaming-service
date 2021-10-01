c35476b157bcbcf909a3b814af16c7c7df5b804d
alina <hodyna.alina@gmail.com>
2021-10-01 12:37:25 +0300

    added path to post method

--- app.js
+++ app.js
@@ -25,10 +25,6 @@ if (process.env.NODE_ENV === 'production') {
         res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
 
     })
-    app.post('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
-
-    })
 }
 
 app.listen(PORT, () => {
